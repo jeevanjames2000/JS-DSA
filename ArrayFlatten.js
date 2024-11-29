0b6ef9f1a2619905e4283ef70ba3fa009ce9dbc5
jeevanjames2000 <jeevanjames2000@gmail.com>
2024-11-29 10:45:08 +0530

    decompress string without regex

--- ArrayFlatten.js
+++ ArrayFlatten.js
@@ -12,3 +12,23 @@ const gen = inorderTraversal([1, [2, 3]]);
 const res = [...gen];
 gen.next().value;
 console.log(res);
+
+//
+//
+//
+// using normal array stack
+
+function generateFlat(arr) {
+  let newArr = [];
+  for (let item in arr) {
+    if (Array.isArray(arr[item])) {
+      newArr = newArr.concat(generateFlat(arr[item]));
+    } else {
+      newArr.push(arr[item]);
+    }
+  }
+  return newArr;
+}
+const data = [1, [2, [3], [4]]];
+const resullt = generateFlat(data);
+console.log(resullt);
--- Strings/stringDecompress.js
+++ Strings/stringDecompress.js
@@ -20,3 +20,31 @@ function decompress(compressed) {
 }
 const compressed = ["a2", "b", "c", "d2"];
 console.log(decompress(compressed));
+
+//
+//
+//
+// my Own version without using regex matchings
+//
+//
+//
+function decompress(str) {
+  let Arr = [];
+  let DecomArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char.length > 1) {
+      const splited = char.slice(0, 1); //char sliced
+      const sliced = char.slice(1); // int sliced
+      for (let j = 0; j < sliced; j++) {
+        DecomArr.push(splited);
+      }
+    } else {
+      DecomArr.push(char);
+    }
+  }
+  return DecomArr;
+}
+const str2 = ["a19", "b10", "c", "d2"];
+
+console.log("dec", decompress(str2));
